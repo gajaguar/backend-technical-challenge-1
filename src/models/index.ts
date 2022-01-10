943f0a4f87ba56328b0a056a49c3ec7212f09eea
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-10 18:38:56 +0000

    🚧 chore(models): add new ones to the index
    - `Author`
    - `Book`
    - `Genre`
    - `Publisher`

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,8 @@
 // models
 
+export * from './author-model'
 export * from './base-model'
+export * from './book-model'
+export * from './genre-model'
+export * from './publisher-model'
 export * from './user-model'
