c8d510e0ca36e4d654bff5e8d2a357089691720e
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-10 22:01:31 +0000

    🚧 chore(routers): add `populate` to the index

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,5 +3,6 @@
 export * from './authors-router'
 export * from './books-router'
 export * from './genres-router'
+export * from './populate-router'
 export * from './publishers-router'
 export * from './users-router'
