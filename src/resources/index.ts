bedb949c34e83c85f1311b15428c4e91c19a894f
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-10 19:18:25 +0000

    🚧 chore(resources): add new ones to the index

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,3 +1,7 @@
 // resources
 
+export * from './authors-resource'
+export * from './books-resource'
+export * from './genres-resource'
+export * from './publishers-resource'
 export * from './users-resource'
